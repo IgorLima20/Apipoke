713e9b3bd01f3134317c1c4d0961e14b5986e7fd
igorlima107 <81596957+igorlima107@users.noreply.github.com>
2022-03-11 22:25:32 -0300

    Atualizado2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  urlImg = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+  listaPokemon = [
+    {numero: 1, nome: 'bulbasaur', tipos: ['grass', 'poison'], foto: '001.png' },
+    {numero: 4, nome: 'charmander', tipos: ['fire'], foto: '004.png' },
+    {numero: 7, nome: 'squirtle', tipos: ['water'], foto: '007.png' },
+    {numero: 25, nome: 'pikachu', tipos: ['eletric'], foto: '025.png' },
+    {numero: 149, nome: 'dragonite', tipos: ['dragon', 'flying'], foto: '149.png' }
+
+  ];
+
   constructor() {}
 
 }
